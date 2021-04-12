2b681e8c7df70a4e2ecb4f3dd4679f8e2d82c95f
Lucien Immink <lucien@arielext.org>
2021-04-12 11:35:04 +0200

    Add tether

--- src/utils/tradepairs.ts
+++ src/utils/tradepairs.ts
@@ -4,11 +4,7 @@ export default [
     brokerUrl: 'https://api.kraken.com/0/public/Ticker?pair=xbteur',
   },
   {
-    name: 'PolkaDot',
-    brokerUrl: 'https://api.kraken.com/0/public/Ticker?pair=doteur',
-  },
-  {
-    name: 'Euro',
-    fiat: true,
+    name: 'Tether',
+    brokerUrl: 'https://api.kraken.com/0/public/Ticker?pair=usdteur',
   },
 ];
